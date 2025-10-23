2b426b5f13874dd654c5bb947b6accae9ab35d9e
Avi Fisher <avifisher82@gmail.com>
2025-10-23 12:50:16 +0300

    Add function

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,3 +1,5 @@
+from utils import back
+
 def create_player(*name: str) -> dict:
 
     try:
@@ -10,6 +12,57 @@ def create_player(*name: str) -> dict:
 
     player = {"name":name,"hand":hand,"won_pile":won_pile}
     return player
+
+def init_game() -> dict:
+    game_dict = {}
+    user_name = input("Enter your name")
+
+    player1 = create_player(user_name)
+    player2 = create_player()
+
+    new_deck = back.create_deck()
+    s_deck = back.shuffle(new_deck)
+    game_dict["deck"] = s_deck
+
+    player1["hand"] = s_deck[:27]
+    game_dict["player_1"] = player1
+
+    player2["hand"] = s_deck[27:]
+    game_dict["player_2"] = player2
+
+    return game_dict
+
+def play_round(player_1: dict, player_2: dict)-> None:
+
+    # global player_1
+    # global player_2
+    #
+
+    cards_play1 = player_1["hand"].pop[0]
+    cards_play2 = player_2["hand"].pop[0]
+
+    win = back.compare_cards(cards_play1[0],cards_play2[0])
+
+    if win == "p1":
+        player_1["won_pile"] += cards_play1,cards_play2
+    if win == "p2":
+        player_2["Won_pile"] += cards_play1,cards_play2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
